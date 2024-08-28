eca6617346c3a09bdaacf5a29040ff9ba359011c
Pawan <pawan.cse2021@gmail.com>
2024-08-28 22:05:55 +0530

    parent child elements in react.

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const heading = React.createElement(
   "This is a heading by using react."
 );
 
+const children = React.createElement("div", {id: "children"}, [heading, heading]);
+const parent = React.createElement("div", {id: "parent"}, children);  
+
+// created element -> 
+
 console.log(heading) // Just a javascript object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // Converts the js object into the browser understandable format.
+root.render(parent); // Converts the js object into the browser understandable format.
