09dd08004697ae58c27a4e52babf02f5380cf44b
Pawan <pawan.cse2021@gmail.com>
2024-09-04 21:14:09 +0530

    Updated the app js file.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is a heading by using react."
-);
 
-const children = React.createElement("div", {id: "children"}, [heading, heading]);
-const parent = React.createElement("div", {id: "parent"}, children);  
+// React.createElement  -> This is basically a js object.
+const heading =  React.createElement("h1", {id: "heading"}, "This is a heading.")
+console.log('heading: ', heading);
 
-// created element -> 
 
-console.log(heading) // Just a javascript object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // Converts the js object into the browser understandable format and replace(not append) on the root element.
+// JSX -> jsx is html-like or XML-like syntax (jsx is not writing html in js)
+const jsxHeading = <h1 id="heading">Heading using jsx</h1>;
+console.log('jsxHeading: ', jsxHeading);
+
+const root = reactDom.createRoot(document.getElementById("root"));
+
+root.render(heading);
